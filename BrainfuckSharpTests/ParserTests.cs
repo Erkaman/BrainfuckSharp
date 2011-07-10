e47fbcd2af233553b449b0a098ba6e1ecae4795b
Eric Arneback <erkastina@hotmail.com>
2011-07-11 00:19:43 +0200

    Accidentally mixed up ParserTests and ScannerTests in their naming. Fixed that.

--- BrainfuckSharpTests/ParserTests.cs
+++ BrainfuckSharpTests/ParserTests.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
 using BrainfuckSharp;
 using NUnit.Framework;
-using System.Collections;
 using System.IO;
+using System;
+using BrainfuckSharp.AbstractSyntaxTree;
 
 namespace BrainfuckSharpTests
 {
@@ -12,22 +13,53 @@ namespace BrainfuckSharpTests
         [Test]
         public void ConstructorTest()
         {
-            string test =
-@"++bla bla fff - , [ ] ] [ ,. < >> < --
-
-k ,
-";
-            IList<char> expected = new List<char>()
-            {
-                '+','+','-',',','[',']',']','[',',','.','<','>','>','<','-','-',
-                ','
-            };
+            string test = @"<>+-.[,+ [++] - ]";
 
             StringReader input = new StringReader(test);
 
-            // scanner = new Scanner(input);
+            Parser parser = new Parser(Scanner.Scan(input) );
 
-            Assert.AreEqual(expected, Scanner.Scan(input));
+            Block result = new Block();
+            result.Statements = new List<Statement>();
+            Action<CommandType> AddCommand = type =>
+                result.Statements.Add(new Command(type));
+
+            // add the first block.
+            AddCommand(CommandType.DecrementPointer);
+            AddCommand(CommandType.IncrementPointer);
+            AddCommand(CommandType.IncrementAtPointer);
+            AddCommand(CommandType.DecrementAtPointer);
+            AddCommand(CommandType.OutputCharacter);
+
+            Block loopBody = new Block();
+                      
+            AddCommand = type =>
+                loopBody.Statements.Add(new Command(type));
+
+            // the first two characters in the loop.
+            AddCommand(CommandType.InputCharacter);
+            AddCommand(CommandType.IncrementAtPointer);
+
+            // the nested loop.
+            Block nestedLoopBody = new Block();
+
+            // the two statements in the nested loop.
+            nestedLoopBody.Statements.Add(new Command(CommandType.IncrementAtPointer));
+            nestedLoopBody.Statements.Add(new Command(CommandType.IncrementAtPointer));
+
+            Loop nestedLoop = new Loop(nestedLoopBody);
+
+            // add the nested loop.
+            loopBody.Statements.Add(nestedLoopBody);
+
+            // add the last command after the nested loop.
+            AddCommand(CommandType.DecrementAtPointer);
+
+            Loop loop = new Loop(loopBody);
+
+            result.Statements.Add(loop);
+
+            Assert.AreEqual(result,parser.Result);
         }
     }
 }
